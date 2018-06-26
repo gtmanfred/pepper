00c533add2425b8b25ff27b2c399f8eb5b738ae1
Daniel Wallace <danielwallace@gtmanfred.com>
2018-06-26 10:12:27 -0500

    trigger build

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,9 +81,7 @@ def pepperconfig(salt_api_port, pepperrc):
     '''.format(salt_api_port))
     with open(pepperrc, 'w') as pepper_file:
         print(config, file=pepper_file)
-
     yield
-
     os.remove(pepperrc)
 
 
